958f6791a48a2934cffbd59c9477e4d2fb4b3ac2
AdamVasina <adamvasina@gmail.com>
2025-05-29 18:34:47 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
         System.out.println(prveCislo * druheCislo);
         System.out.println(prveCislo / druheCislo);*/
 
-        //ULOHA - UZIVATEL ZADA SLOVO - VO VYPISE BUDU VSETKY PISMENA VELKYM
+        /*//ULOHA - UZIVATEL ZADA SLOVO - VO VYPISE BUDU VSETKY PISMENA VELKYM
         Scanner mujScanner = new Scanner(System.in);
         System.out.println("Zadaj svoje slovo:");
         String slovo = mujScanner.nextLine();
@@ -58,10 +58,45 @@ public class Main {
         System.out.println(slovo.toUpperCase());
         System.out.println(slovo.length());
         System.out.println(slovo.isEmpty());
-        System.out.println(slovo.indexOf('j'));
+        System.out.println(slovo.indexOf('j'));*/
 
+        //Parsovanie -prevody medzi typmi
+        //int prevedeneCilo = Integer.parseInt('5');
 
+        /*int prveCislo = 25;
+        int druheCislo = 2;
+        //prveCislo = prveCislo + druheCislo;
+        prveCislo += druheCislo;*/
 
+        /*int prvCislo;
+        int druhCislo;
+        Scanner mujScanner = new Scanner(System.in);
+
+        System.out.println ("Zadaj prve cislo");
+        prveCislo = mujScanner.nextInt();
+
+        System.out.println ("Zadaj druhe cislo");
+        druheCislo = mujScanner.nextInt();
+
+        // ROVNAKE ULOHY SA DAJU SA ZAPISAT ROZNE
+
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(prveCislo * druheCislo);
+        System.out.println(prveCislo / druheCislo);
+
+        System.out.println(prveCislo > druheCislo);*/
+
+        //APLIKACIA NA KONTROLU VEKU (FEATURE) - ak bude uzivate starsi 18 a viac vypis do kozole - Vitaj
+
+        int vek;
+        Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoj vek:");
+        vek =mujScanner.nextInt();
+
+        if(vek >= 18) {
+            System.out.println("Uzivatel je dospely");
+        }
 
 
 
